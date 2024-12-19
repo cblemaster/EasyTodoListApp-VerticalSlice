350e922442f62b2468249e910fdde439620c1b23
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-19 18:46:31 -0500

    added app db context

---
+++ EasyTodoListApp/Infrastructure/AppDbContext.cs
@@ -0,0 +1,24 @@
+
+using EasyTodoListApp.Todos.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyTodoListApp.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlite("Data Source=easytodolist.dat");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity.ToTable("Todo");
+            entity.Property(e => e.Description)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+}
