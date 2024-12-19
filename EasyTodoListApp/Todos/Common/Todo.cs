0ba7485c7e4eee530f16457b70f0fc077799e0c9
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-19 18:32:22 -0500

    added todo model

---
+++ EasyTodoListApp/Todos/Common/Todo.cs
@@ -0,0 +1,15 @@
+﻿
+namespace EasyTodoListApp.Todos.Common;
+
+public class Todo
+{
+    public string Description { get; set; } = string.Empty;
+    public DateOnly? DueDate { get; set; }
+    public bool IsImportant { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public Guid Id { get; set; }
+}
+
+// TODO: Create separate domain and persistence models
