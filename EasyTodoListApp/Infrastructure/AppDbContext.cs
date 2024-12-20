de9aa00fd272459a6d7bd9d3285ad2e95bc74175
Brian LeMaster <brian.lemaster@outlook.com>
2024-12-19 19:53:44 -0500

    added initial migration

--- EasyTodoListApp/Infrastructure/AppDbContext.cs
+++ EasyTodoListApp/Infrastructure/AppDbContext.cs
@@ -6,6 +6,8 @@ namespace EasyTodoListApp.Infrastructure;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext() { }
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     
     public DbSet<Todo> Todos { get; set; }
---
+++ EasyTodoListApp/Migrations/20241220004811_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyTodoListApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", unicode: false, maxLength: 100, nullable: false),
+                    DueDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    IsImportant = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todo");
+        }
+    }
+}
